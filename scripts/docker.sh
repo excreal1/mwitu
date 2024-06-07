ce8b3ccb32a56b35670f662ace2d4aab425b0b52
Not Bruce Wayne <148652451+excreal1@users.noreply.github.com>
2024-06-07 10:40:20 +0000

    Update docker.sh

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,14 +7,14 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Add the current user to the docker group
-sudo apt update && sudo apt install docker docker-compose
-sudo usermod -aG docker $USER
+apt update && sudo apt install docker docker-compose
+usermod -aG docker $USER
 
 # Create a directory for Nginx Proxy Manager
-sudo mkdir /opt/nginx-proxy-manager
+mkdir /opt/nginx-proxy-manager
 
 # Create a Docker Compose file
-sudo cat <<EOT >> /opt/nginx-proxy-manager/docker-compose.yml
+cat <<EOT >> /opt/nginx-proxy-manager/docker-compose.yml
 version: '3'
 
 services:
@@ -51,8 +51,7 @@ EOT
 
 # Change permissions for the Docker Compose file
 
-sudo chmod 664 /opt/*
-sudo chmod 644 /opt/nginx-proxy-manager/docker-compose.yml
+chmod 644 /opt/nginx-proxy-manager/docker-compose.yml
 
 # Start Nginx Proxy Manager
 cd /opt/nginx-proxy-manager
